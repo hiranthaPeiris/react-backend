08cb26aea453976a21d6b212bd4b30bd21ea0ee5
Hirantha <hiranthapeiris404@gmail.com>
2020-01-27 23:55:21 +0530

    docker-compse-up

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("hello node");
+//conect mongo
+mongoose
+  .connect("mongodb://mongo:27017/react-backend", { useNewUrlParser: true })
+  .then(() => console.log("Mongo Connected"))
+  .catch(err => console.log("error connecting" + err));
+
+app.get("/", (req, res) => {
+  res.send("hello node");
 });
 
-app.listen(5000, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
